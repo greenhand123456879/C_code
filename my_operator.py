0352314e8e73f80e211836e2e0a271fcdf8166a8
LL <1163890711@qq.com>
2022-10-06 16:49:48 +0800

    old rep2

---
+++ my_operator.py
@@ -0,0 +1,47 @@
+# + - * / % **(exp) //(除法向下取整) 算术运算符
+# be care the priority
+
+# a = 4
+# b = -1.1
+# print(a + b)
+# print(a - b)
+#
+# print(a / b)  # 0.5
+# print(a * b)
+# print(a % b)
+# print(a // b)  # 向下取整
+
+###############################################################
+# == != > < >= <= 比较运算符
+
+# a, b = 10, 5
+# print(a, b)
+# print(a == b)
+# print(a != b)
+# print(a > b)
+# print(a < b)
+# print(a <= b)
+# print(a >= b)
+
+###############################################################
+# and or not logic运算符
+# 优先级 ()-->not-->and-->or
+
+# print(1 and 2)
+# print(1 and 5)
+# print(1 and -5)
+# print(0 and 100)
+# print(1 > 2 and 2)
+# print(1 > 2 or 2)
+# print(0 or 3)
+# print(1 or 3)
+# s1, s2 = ,
+# print(s1 and not s2 or not s1 and s2)
+# () and not() or not() and ()
+
+###############################################################
+# += -= /= *= **= //= %= 赋值运算
+a = 10
+c = 10
+a+=c
+print(a)
